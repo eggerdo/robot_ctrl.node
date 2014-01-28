8309b7fa47e23330d6444aa2c3ccad6451e34cdb
Dominik <dominik@dobots.nl>
2014-01-28 10:15:32 +0100

    bugfix start script

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ gnome-terminal \
 	--tab -e "leapd" \
 	--tab -e "node robot_ctrl.js"  \
 	--tab -e "sh zmqwebbridge.sh" \
-	--tab -e "node ../robot_server/robot_server.js"
+	--tab -e "node ../robot_server.node/robot_server.js"
